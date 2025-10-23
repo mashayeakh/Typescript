dca1b10dd04db777d61e54d8ef601fed3b2cedf7
mashayeakh <islammasayekh@gmail.com>
2025-10-24 00:48:26 +0600

    done with the types

--- 01Intro/variableMe.ts
+++ 01Intro/variableMe.ts
@@ -3,7 +3,62 @@ console.log(greetings);
 
 //number
 let age:number = 25;
+// age.toFixed(2);
 console.log(age);
+
+//*somethings you dont need to expliceitly define the type. ts autamatically infers the type
+let price = "hola";
+console.log(price)
+
+//boolean
+let isLoggedIn:boolean = false;
+// isLoggedIn.
+
+
+
+// any 
+// let hero ;
+
+// function getHero (){
+//     return "thor";
+// }
+
+// hero = getHero();
+
+let hero: string;
+function getHero (){
+    return "thor";
+}
+hero = getHero();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //TEMP SOLUTION TO INGORE THE ERR
 export {};
