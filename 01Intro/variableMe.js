434f94678b59a1e2061d7d87a51ec72056cc0caa
mashayeakh <islammasayekh@gmail.com>
2025-10-23 23:48:33 +0600

    first commit

---
+++ 01Intro/variableMe.js
@@ -0,0 +1,2 @@
+var greetings = "Hello, Masayeakh!!!";
+console.log(greetings);
---
+++ 01Intro/variableMe.ts
@@ -0,0 +1,7 @@
+let greetings:string = "Hello, Masayeakh!!!";
+console.log(greetings);
+
+
+
+//TEMP SOLUTION TO INGORE THE ERR
+export {};
