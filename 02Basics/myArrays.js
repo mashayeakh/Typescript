5bf90223b1975ce0bbfb2f8a27fe5fca3861dd70
mashayeakh <islammasayekh@gmail.com>
2025-10-26 14:24:31 +0600

    started with arr

---
+++ 02Basics/myArrays.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//!Defining string type arr
+var superHeroes = [];
+var superNumbers = [];
+//another way
+var heroNumbers = [];
+superHeroes.push("spiderman, ironman");
+superNumbers.push(34);
+heroNumbers.push(23);
+console.log(superHeroes);
+console.log(superNumbers);
+console.log(heroNumbers);
+//array of objects
+var userArray = [];
+//pushing values
+userArray.push({ name: "john", age: 25, email: "jhon@gmail.com" });
+console.log(userArray);
+//*Arrays of arrays
+var models = [
+    ["car", "bike"],
+    ["bus", "truck"]
+];
+console.log(models);
