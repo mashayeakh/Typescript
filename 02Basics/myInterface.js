69639f899b8a1318fd5243c3559fd645062d9770
mashayeakh <islammasayekh@gmail.com>
2025-10-27 18:37:43 +0600

    worked on interface

---
+++ 02Basics/myInterface.js
@@ -0,0 +1,36 @@
+//! Interface - defining the structure of an object
+//! it tells ts what properties an object should have and their types
+//*Crteating an object that adheres to the User interface
+var user1 = {
+    name: "MIP",
+    age: 30,
+    isActive: true,
+};
+console.log(user1);
+var phone = {
+    id: 1,
+    name: "iphone 14",
+};
+console.log(phone);
+var myCar = {
+    vin: "123ABC",
+    brand: "Toyota",
+    model: "Corolla",
+};
+console.log(myCar);
+var auth = function (u, p) {
+    if (u === "admin" && p === "123") {
+        return "WELCOME";
+    }
+    return "Access Denied";
+};
+console.log(auth("admin", "123")); //true
+//obj
+var result = {
+    val1: 23,
+    val2: 32,
+    operation: function () {
+        return this.val1 + this.val2;
+    }
+};
+console.log(result.operation());
---
+++ 02Basics/myInterface.ts
@@ -0,0 +1,81 @@
+//! Interface - defining the structure of an object
+//! it tells ts what properties an object should have and their types
+
+//*Example 
+interface User{
+    name: string;
+    age: number;
+    isActive: boolean;
+}
+
+//*Crteating an object that adheres to the User interface
+let user1:User={
+    name:"MIP",
+    age:30,
+    isActive:true, 
+}
+console.log(user1)
+
+//*Optinal Properties
+interface product {
+    id: number;
+    name:string;
+    desc?:string;
+}
+
+let phone:product ={
+    id:1, 
+    name:"iphone 14",
+}
+console.log(phone)
+
+
+//* Readonly properties
+interface Car {
+    readonly vin:string;
+    brand:string;
+    model:string;
+}
+let myCar:Car={
+    vin:"123ABC",
+    brand:"Toyota",
+    model:"Corolla",
+}
+console.log(myCar)  
+//? myCar.vin="456DEF"; //Error: Cannot assign to 'vin' because it is a read-only property
+
+
+//*interface with Function 
+interface Login{
+    (username:string, psw:string):string // here this login interface defines a func, taking 2 arguments/parameter and it should return a boolean value like true or false. 
+}
+
+let auth:Login=(u, p)=>{
+    if(u==="admin"&& p==="123"){
+        return "WELCOME"
+    }
+    return "Access Denied"
+}
+
+console.log(auth("admin", "123")); //true
+
+
+//* i can define function as well in an interface
+
+interface MathOp{
+    val1:number, 
+    val2:number,
+
+    operation():(number);
+}
+
+//obj
+let result:MathOp={
+    val1: 23,
+    val2: 32,
+    operation(){
+        return this.val1 + this.val2;
+    }
+}
+
+console.log(result.operation());
