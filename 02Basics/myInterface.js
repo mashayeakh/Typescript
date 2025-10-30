73503ee95bdd4dd6357010d04588350c40aa8162
mashayeakh <islammasayekh@gmail.com>
2025-10-31 01:48:58 +0600

    worked on interface and its inheritence

--- 02Basics/myInterface.js
+++ 02Basics/myInterface.js
@@ -1,5 +1,5 @@
 //! Interface - defining the structure of an object
-//! it tells ts what properties an object should have and their types
+//! i t tellsts what properties an object should have and their types
 //*Crteating an object that adheres to the User interface
 var user1 = {
     name: "MIP",
@@ -18,13 +18,13 @@ var myCar = {
     model: "Corolla",
 };
 console.log(myCar);
-var auth = function (u, p) {
+var auth = function (u, p, name) {
     if (u === "admin" && p === "123") {
-        return "WELCOME";
+        return "WELCOME ".concat(name);
     }
     return "Access Denied";
 };
-console.log(auth("admin", "123")); //true
+console.log(auth("admin", "123", "Masayeakh")); //true
 //obj
 var result = {
     val1: 23,
@@ -47,3 +47,19 @@ var mip = {
 };
 console.log(mip.startTrail());
 console.log(mip.getCoupon("BLACKFRIDAY"));
+//one onj
+var post1 = {
+    title: "Understanding TypeScript Interfaces",
+    content: "Interfaces in TypeScript are powerful tools for defining the structure of objects...",
+    author: "Masayeakh",
+    summarize: function () {
+        return "This blog post explains the concept of interfaces in TypeScript.";
+    },
+    publishedIn: "IEEE Journal",
+    personalNote: "This is my personal blog post about TypeScript interfaces.......",
+    loggedDetails: function () {
+        var _a;
+        return "Post titled: \"".concat(post1.title, "\"\n \n        authored by: ").concat(post1.author, "\n \n        content: ").concat(post1.content, "\n \n        tags: ").concat(((_a = post1.tags) === null || _a === void 0 ? void 0 : _a.join(", ")) || "No tags", "\n \n        summary\"").concat(post1.summarize(), "\"\n \n        published in: ").concat(post1.publishedIn, "\n\n        personal note: ").concat(post1.personalNote, "\n        ");
+    }
+};
+console.log(post1.loggedDetails());
--- 02Basics/myInterface.ts
+++ 02Basics/myInterface.ts
@@ -47,17 +47,17 @@ console.log(myCar)
 
 //*interface with Function 
 interface Login{
-    (username:string, psw:string):string // here this login interface defines a func, taking 2 arguments/parameter and it should return a boolean value like true or false. 
+    (username:string, psw:string, name:string):string // here this login interface defines a func, taking 2 arguments/parameter and it should return a boolean value like true or false. 
 }
 
-let auth:Login=(u, p)=>{
+let auth:Login=(u, p, name)=>{
     if(u==="admin"&& p==="123"){
-        return "WELCOME"
+        return `WELCOME ${name}`;
     }
     return "Access Denied"
 }
 
-console.log(auth("admin", "123")); //true
+console.log(auth("admin", "123", "Masayeakh")); //true
 
 
 //* i can define function as well in an interface
@@ -107,3 +107,54 @@ const mip:UserProfile={
 
 console.log(mip.startTrail())
 console.log(mip.getCoupon("BLACKFRIDAY"))
+
+
+//? lets create anotehr interface for a blog post
+interface BlogPost{
+    title:string,
+    content:string,
+    author:string,
+    tags?:string[],
+
+    //function to summarize the blog post
+    summarize():string;
+    loggedDetails():string;
+}
+
+
+//one onj
+const post1:PersonalBlog={
+    title:"Understanding TypeScript Interfaces",
+    content:"Interfaces in TypeScript are powerful tools for defining the structure of objects...",
+    author:"Masayeakh",
+    summarize:()=>{
+        return "This blog post explains the concept of interfaces in TypeScript.";
+    },
+    publishedIn:"IEEE Journal",
+    personalNote:"This is my personal blog post about TypeScript interfaces.......",
+
+    loggedDetails:()=>{
+        return `Post titled: "${post1.title}"\n 
+        authored by: ${post1.author}\n 
+        content: ${post1.content}\n 
+        tags: ${post1.tags?.join(", ") || "No tags"}\n 
+        summary"${post1.summarize()}"\n 
+        published in: ${post1.publishedIn}\n
+        personal note: ${post1.personalNote}
+        `;
+    }
+}
+
+console.log(post1.loggedDetails());
+
+//?Lets create the same interface with another property - this is called reopening of the interface 
+interface BlogPost{
+    publishedIn:string;
+}
+
+
+//? We can also extend interfaces - this is called interface inheritance
+interface PersonalBlog extends BlogPost{
+    personalNote:string;
+    // getPersonalNote():string;
+}
